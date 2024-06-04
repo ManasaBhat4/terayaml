5212425352268985e316855194602f213b87cdf0
Manasa Bhat <113411403+ManasaBhat4@users.noreply.github.com>
2024-06-04 18:07:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
 }
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-bucket"
+  acl    = "private"
+}
